3007c1668e864656a83256ab319f9cdad44d9cea
Jon Gjengset <jon@thesquareplanet.com>
2015-04-15 13:39:00 -0400

    Improve method naming

---
+++ cucache/execute.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"cuckood"
+	"encoding/binary"
+	"fmt"
+	"math"
+	"time"
+
+	gomem "github.com/dustin/gomemcached"
+)
+
+func tm(i uint32) (t time.Time) {
+	if i == 0 {
+		return
+	}
+
+	if i < 60*60*24*30 {
+		t = time.Now().Add(time.Duration(i) * time.Second)
+	} else {
+		t = time.Unix(int64(i), 0)
+	}
+	return
+}
+
+func req2res(req *gomem.MCRequest) (res *gomem.MCResponse) {
+	res = new(gomem.MCResponse)
+
+	res.Opaque = req.Opaque
+	res.Opcode = req.Opcode
+
+	switch req.Opcode {
+	case gomem.GET, gomem.GETQ, gomem.GETK, gomem.GETKQ:
+		res.Status = gomem.KEY_ENOENT
+		v, ok := c.Get(req.Key)
+		if ok {
+			res.Status = gomem.SUCCESS
+			res.Extras = make([]byte, 4)
+			binary.BigEndian.PutUint32(res.Extras, v.Flags)
+			res.Cas = v.Casid
+			res.Body = v.Bytes
+
+			if req.Opcode == gomem.GETK || req.Opcode == gomem.GETKQ {
+				res.Key = req.Key
+			}
+		}
+	case gomem.SET, gomem.SETQ,
+		gomem.ADD, gomem.ADDQ,
+		gomem.REPLACE, gomem.REPLACEQ:
+
+		flags := binary.BigEndian.Uint32(req.Extras[0:4])
+		expiry := tm(binary.BigEndian.Uint32(req.Extras[4:8]))
+		var v cuckoo.MemopRes
+		switch req.Opcode {
+		case gomem.SET, gomem.SETQ:
+			if req.Cas == 0 {
+				v = c.Set(req.Key, req.Body, flags, expiry)
+			} else {
+				v = c.CAS(req.Key, req.Body, flags, expiry, req.Cas)
+			}
+		case gomem.ADD, gomem.ADDQ:
+			v = c.Add(req.Key, req.Body, flags, expiry)
+		case gomem.REPLACE, gomem.REPLACEQ:
+			if req.Cas == 0 {
+				v = c.Replace(req.Key, req.Body, flags, expiry)
+			} else {
+				v = c.CAS(req.Key, req.Body, flags, expiry, req.Cas)
+			}
+		}
+
+		switch v.T {
+		case cuckoo.STORED:
+			res.Status = gomem.SUCCESS
+			res.Cas = v.V.(uint64)
+		case cuckoo.NOT_STORED:
+			res.Status = gomem.NOT_STORED
+		case cuckoo.NOT_FOUND:
+			res.Status = gomem.KEY_ENOENT
+		case cuckoo.EXISTS:
+			res.Status = gomem.KEY_EEXISTS
+		case cuckoo.SERVER_ERROR:
+			res.Status = gomem.ENOMEM
+			fmt.Println(v.V.(error))
+		default:
+			wtf(req, v)
+		}
+	case gomem.DELETE, gomem.DELETEQ:
+		v := c.Delete(req.Key, req.Cas)
+
+		switch v.T {
+		case cuckoo.STORED:
+			res.Status = gomem.SUCCESS
+		case cuckoo.NOT_FOUND:
+			res.Status = gomem.KEY_ENOENT
+		case cuckoo.EXISTS:
+			res.Status = gomem.KEY_EEXISTS
+		default:
+			wtf(req, v)
+		}
+	case gomem.INCREMENT, gomem.INCREMENTQ,
+		gomem.DECREMENT, gomem.DECREMENTQ:
+
+		by := binary.BigEndian.Uint64(req.Extras[0:8])
+		def := binary.BigEndian.Uint64(req.Extras[8:16])
+		exp := tm(binary.BigEndian.Uint32(req.Extras[16:20]))
+
+		if binary.BigEndian.Uint32(req.Extras[16:20]) == 0xffffffff {
+			exp = time.Unix(math.MaxInt64, 0)
+		}
+
+		var v cuckoo.MemopRes
+		if req.Opcode == gomem.INCREMENT || req.Opcode == gomem.INCREMENTQ {
+			v = c.Incr(req.Key, by, def, exp)
+		} else {
+			v = c.Decr(req.Key, by, def, exp)
+		}
+
+		switch v.T {
+		case cuckoo.STORED:
+			res.Status = gomem.SUCCESS
+			cv := v.V.(cuckoo.CasVal)
+			res.Cas = cv.Casid
+			res.Body = make([]byte, 8)
+			binary.BigEndian.PutUint64(res.Body, cv.NewVal)
+		case cuckoo.CLIENT_ERROR:
+			res.Status = gomem.DELTA_BADVAL
+		case cuckoo.NOT_FOUND:
+			res.Status = gomem.KEY_ENOENT
+		default:
+			wtf(req, v)
+		}
+	case gomem.QUIT, gomem.QUITQ:
+		return
+	case gomem.FLUSH, gomem.FLUSHQ:
+		at := tm(binary.BigEndian.Uint32(req.Extras[0:4]))
+		if at.IsZero() {
+			at = time.Now()
+		}
+		c.TouchAll(at)
+		res.Status = gomem.SUCCESS
+	case gomem.NOOP:
+		res.Status = gomem.SUCCESS
+	case gomem.VERSION:
+		res.Status = gomem.SUCCESS
+		// TODO: res.Body =
+	case gomem.APPEND, gomem.APPENDQ,
+		gomem.PREPEND, gomem.PREPENDQ:
+
+		var v cuckoo.MemopRes
+		switch req.Opcode {
+		case gomem.APPEND, gomem.APPENDQ:
+			v = c.Append(req.Key, req.Body, req.Cas)
+		case gomem.PREPEND, gomem.PREPENDQ:
+			v = c.Prepend(req.Key, req.Body, req.Cas)
+		}
+
+		switch v.T {
+		case cuckoo.STORED:
+			res.Status = gomem.SUCCESS
+		case cuckoo.EXISTS:
+			res.Status = gomem.KEY_EEXISTS
+		case cuckoo.NOT_FOUND:
+			res.Status = gomem.KEY_ENOENT
+		default:
+			wtf(req, v)
+		}
+	default:
+		res.Status = gomem.UNKNOWN_COMMAND
+	}
+
+	switch res.Status {
+	case gomem.KEY_ENOENT:
+		res.Body = []byte("Not found")
+	case gomem.KEY_EEXISTS:
+		res.Body = []byte("Data exists for key.")
+	case gomem.NOT_STORED:
+		res.Body = []byte("Not stored.")
+	case gomem.ENOMEM:
+		res.Body = []byte("Out of memory")
+	case gomem.UNKNOWN_COMMAND:
+		res.Body = []byte("Unknown command")
+	case gomem.EINVAL:
+		res.Body = []byte("Invalid arguments")
+	case gomem.E2BIG:
+		res.Body = []byte("Too large.")
+	case gomem.DELTA_BADVAL:
+		res.Body = []byte("Non-numeric server-side value for incr or decr")
+	}
+
+	return
+}
--- cucache/main.go
+++ cucache/main.go
@@ -9,7 +9,6 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"math"
 	"net"
 	"os"
 	"os/signal"
@@ -17,7 +16,6 @@ import (
 	"strconv"
 	"sync"
 	"syscall"
-	"time"
 
 	gomem "github.com/dustin/gomemcached"
 )
@@ -104,185 +102,7 @@ func wtf(req *gomem.MCRequest, v cuckoo.MemopRes) {
 	panic(fmt.Sprintf("unexpected result when handling %v: %v\n", req.Opcode, v))
 }
 
-func tm(i uint32) (t time.Time) {
-	if i == 0 {
-		return
-	}
-
-	if i < 60*60*24*30 {
-		t = time.Now().Add(time.Duration(i) * time.Second)
-	} else {
-		t = time.Unix(int64(i), 0)
-	}
-	return
-}
-
-func req2res(req *gomem.MCRequest) (res gomem.MCResponse) {
-	res.Opaque = req.Opaque
-	res.Opcode = req.Opcode
-
-	switch req.Opcode {
-	case gomem.GET, gomem.GETQ, gomem.GETK, gomem.GETKQ:
-		res.Status = gomem.KEY_ENOENT
-		v, ok := c.Get(req.Key)
-		if ok {
-			res.Status = gomem.SUCCESS
-			res.Extras = make([]byte, 4)
-			binary.BigEndian.PutUint32(res.Extras, v.Flags)
-			res.Cas = v.Casid
-			res.Body = v.Bytes
-
-			if req.Opcode == gomem.GETK || req.Opcode == gomem.GETKQ {
-				res.Key = req.Key
-			}
-		}
-	case gomem.SET, gomem.SETQ,
-		gomem.ADD, gomem.ADDQ,
-		gomem.REPLACE, gomem.REPLACEQ:
-
-		flags := binary.BigEndian.Uint32(req.Extras[0:4])
-		expiry := tm(binary.BigEndian.Uint32(req.Extras[4:8]))
-		var v cuckoo.MemopRes
-		switch req.Opcode {
-		case gomem.SET, gomem.SETQ:
-			if req.Cas == 0 {
-				v = c.Set(req.Key, req.Body, flags, expiry)
-			} else {
-				v = c.CAS(req.Key, req.Body, flags, expiry, req.Cas)
-			}
-		case gomem.ADD, gomem.ADDQ:
-			v = c.Add(req.Key, req.Body, flags, expiry)
-		case gomem.REPLACE, gomem.REPLACEQ:
-			if req.Cas == 0 {
-				v = c.Replace(req.Key, req.Body, flags, expiry)
-			} else {
-				v = c.CAS(req.Key, req.Body, flags, expiry, req.Cas)
-			}
-		}
-
-		switch v.T {
-		case cuckoo.STORED:
-			res.Status = gomem.SUCCESS
-			res.Cas = v.V.(uint64)
-		case cuckoo.NOT_STORED:
-			res.Status = gomem.NOT_STORED
-		case cuckoo.NOT_FOUND:
-			res.Status = gomem.KEY_ENOENT
-		case cuckoo.EXISTS:
-			res.Status = gomem.KEY_EEXISTS
-		case cuckoo.SERVER_ERROR:
-			res.Status = gomem.ENOMEM
-			fmt.Println(v.V.(error))
-		default:
-			wtf(req, v)
-		}
-	case gomem.DELETE, gomem.DELETEQ:
-		v := c.Delete(req.Key, req.Cas)
-
-		switch v.T {
-		case cuckoo.STORED:
-			res.Status = gomem.SUCCESS
-		case cuckoo.NOT_FOUND:
-			res.Status = gomem.KEY_ENOENT
-		case cuckoo.EXISTS:
-			res.Status = gomem.KEY_EEXISTS
-		default:
-			wtf(req, v)
-		}
-	case gomem.INCREMENT, gomem.INCREMENTQ,
-		gomem.DECREMENT, gomem.DECREMENTQ:
-
-		by := binary.BigEndian.Uint64(req.Extras[0:8])
-		def := binary.BigEndian.Uint64(req.Extras[8:16])
-		exp := tm(binary.BigEndian.Uint32(req.Extras[16:20]))
-
-		if binary.BigEndian.Uint32(req.Extras[16:20]) == 0xffffffff {
-			exp = time.Unix(math.MaxInt64, 0)
-		}
-
-		var v cuckoo.MemopRes
-		if req.Opcode == gomem.INCREMENT || req.Opcode == gomem.INCREMENTQ {
-			v = c.Incr(req.Key, by, def, exp)
-		} else {
-			v = c.Decr(req.Key, by, def, exp)
-		}
-
-		switch v.T {
-		case cuckoo.STORED:
-			res.Status = gomem.SUCCESS
-			cv := v.V.(cuckoo.CasVal)
-			res.Cas = cv.Casid
-			res.Body = make([]byte, 8)
-			binary.BigEndian.PutUint64(res.Body, cv.NewVal)
-		case cuckoo.CLIENT_ERROR:
-			res.Status = gomem.DELTA_BADVAL
-		case cuckoo.NOT_FOUND:
-			res.Status = gomem.KEY_ENOENT
-		default:
-			wtf(req, v)
-		}
-	case gomem.QUIT, gomem.QUITQ:
-		return
-	case gomem.FLUSH, gomem.FLUSHQ:
-		at := tm(binary.BigEndian.Uint32(req.Extras[0:4]))
-		if at.IsZero() {
-			at = time.Now()
-		}
-		c.TouchAll(at)
-		res.Status = gomem.SUCCESS
-	case gomem.NOOP:
-		res.Status = gomem.SUCCESS
-	case gomem.VERSION:
-		res.Status = gomem.SUCCESS
-		// TODO: res.Body =
-	case gomem.APPEND, gomem.APPENDQ,
-		gomem.PREPEND, gomem.PREPENDQ:
-
-		var v cuckoo.MemopRes
-		switch req.Opcode {
-		case gomem.APPEND, gomem.APPENDQ:
-			v = c.Append(req.Key, req.Body, req.Cas)
-		case gomem.PREPEND, gomem.PREPENDQ:
-			v = c.Prepend(req.Key, req.Body, req.Cas)
-		}
-
-		switch v.T {
-		case cuckoo.STORED:
-			res.Status = gomem.SUCCESS
-		case cuckoo.EXISTS:
-			res.Status = gomem.KEY_EEXISTS
-		case cuckoo.NOT_FOUND:
-			res.Status = gomem.KEY_ENOENT
-		default:
-			wtf(req, v)
-		}
-	default:
-		res.Status = gomem.UNKNOWN_COMMAND
-	}
-
-	switch res.Status {
-	case gomem.KEY_ENOENT:
-		res.Body = []byte("Not found")
-	case gomem.KEY_EEXISTS:
-		res.Body = []byte("Data exists for key.")
-	case gomem.NOT_STORED:
-		res.Body = []byte("Not stored.")
-	case gomem.ENOMEM:
-		res.Body = []byte("Out of memory")
-	case gomem.UNKNOWN_COMMAND:
-		res.Body = []byte("Unknown command")
-	case gomem.EINVAL:
-		res.Body = []byte("Invalid arguments")
-	case gomem.E2BIG:
-		res.Body = []byte("Too large.")
-	case gomem.DELTA_BADVAL:
-		res.Body = []byte("Non-numeric server-side value for incr or decr")
-	}
-
-	return
-}
-
-func handle1(in <-chan *gomem.MCRequest, out chan<- *gomem.MCResponse) {
+func execute(in <-chan *gomem.MCRequest, out chan<- *gomem.MCResponse) {
 	mx := new(sync.Mutex)
 
 	for req := range in {
@@ -308,14 +128,15 @@ func handle1(in <-chan *gomem.MCRequest, out chan<- *gomem.MCResponse) {
 
 		mx.Lock()
 		go func() {
-			out <- &res
+			out <- res
 			mx.Unlock()
 		}()
 	}
 	close(out)
 }
 
-func handle2(in <-chan *gomem.MCResponse, out *bufio.Writer) {
+func writeback(in <-chan *gomem.MCResponse, out_ io.Writer) {
+	out := bufio.NewWriter(out_)
 	mx := new(sync.Mutex)
 
 	for res := range in {
@@ -379,18 +200,12 @@ func handle2(in <-chan *gomem.MCResponse, out *bufio.Writer) {
 		case gomem.UNKNOWN_COMMAND:
 			out.Write([]byte("ERROR\r\n"))
 		}
+		resP.Put(res)
 	}
 }
 
-func deal(in_ io.Reader, out_ io.Writer) {
+func parse(in_ io.Reader, out chan<- *gomem.MCRequest) {
 	in := bufio.NewReader(in_)
-	out := bufio.NewWriter(out_)
-
-	dispatch := make(chan *gomem.MCRequest, 50)
-	defer close(dispatch)
-	bridge := make(chan *gomem.MCResponse, 50)
-	go handle1(dispatch, bridge)
-	go handle2(bridge, out)
 
 	for {
 		b, err := in.Peek(1)
@@ -402,7 +217,7 @@ func deal(in_ io.Reader, out_ io.Writer) {
 			return
 		}
 
-		var req gomem.MCRequest
+		req := new(gomem.MCRequest)
 		if b[0] == gomem.REQ_MAGIC {
 			_, err := req.Receive(in, nil)
 			if err != nil {
@@ -423,12 +238,21 @@ func deal(in_ io.Reader, out_ io.Writer) {
 				return
 			}
 
-			req, err = text.ToMCRequest(cmd, in)
+			*req, err = text.ToMCRequest(cmd, in)
 			req.Opaque = 0xffffffff
 		}
 
-		dispatch <- &req
+		out <- req
 	}
+	close(out)
+}
+
+func setup(in io.Reader, out io.Writer) {
+	dispatch := make(chan *gomem.MCRequest, 50)
+	bridge := make(chan *gomem.MCResponse, 50)
+	go execute(dispatch, bridge)
+	go writeback(bridge, out)
+	parse(in, dispatch)
 }
 
 func replyTo(in []byte, to *net.UDPAddr) {
@@ -440,7 +264,7 @@ func replyTo(in []byte, to *net.UDPAddr) {
 	defer u.Close()
 
 	var o bytes.Buffer
-	deal(bytes.NewBuffer(in), &o)
+	setup(bytes.NewBuffer(in), &o)
 	_, err = u.WriteTo(o.Bytes(), to)
 	if err != nil {
 		fmt.Println(err)
@@ -448,6 +272,6 @@ func replyTo(in []byte, to *net.UDPAddr) {
 }
 
 func handleConnection(c net.Conn) {
-	deal(c, c)
+	setup(c, c)
 	c.Close()
 }
