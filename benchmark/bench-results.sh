e5e6b76feb1768761b8bf043c7b830aa8d076fe5
Jon Gjengset <jon@thesquareplanet.com>
2015-04-29 17:16:51 -0400

    Show all memcached plots, diff type by dt

--- benchmark/bench-results.sh
+++ benchmark/bench-results.sh
@@ -11,37 +11,40 @@ plot
 EOF
 
 i=2
+mi=9
 seen_mc=0
 for f in "${versions[@]}"; do
-	dt=$i
-
 	# Tidy up title
 	t="$f"
 	t=$(echo "$t" | perl -pe 's/-(tom|ben)(-.*|$)/\2 on \1/')
 	t=$(echo "$t" | sed 's/^exp-//')
+	t=$(echo "$t" | sed 's/^-memcached-//')
+	t=$(echo "$t" | perl -pe 's/^(.*)-memcached-/memcached /')
 
 	# Is this a memcached entry?
+	mc=0
 	echo "$f" | grep memcached >/dev/null
-	[ $? -eq 0 ] && dt=1 && t="Memcached"
+	[ $? -eq 0 ] && mc=1
 
-	# No title for later memcached entries
-	[ $dt -eq 1 ] && [ $seen_mc -eq 1 ] && t=""
-	[ $dt -eq 1 ] && seen_mc=1
+	# line type
+	lt=$i
+	[ $mc -eq 1 ] && lt=$mi
 
 	# Add plot lines; tt is needed to not print ' get hit' for hidden
 	# memcached lines
 	tt=""
-	[ -n "$t" ] && tt="get hit ($t)"
-	#out+=" \"< grep '$f' cucache.dat | perl -pe 's/^.*?-(\\\\d+)/\\\\1/' | grep Gets\" u 1:3 dt $dt lt 1 t '$tt' w linespoints,"
-	[ -n "$t" ] && tt="set ($t)"
-	out+=" \"< grep '$f' cucache.dat | perl -pe 's/^.*?-(\\\\d+)/\\\\1/' | grep Sets\" u 1:3 dt $dt lt 2 t '$tt' w linespoints,"
-	[ -n "$t" ] && tt="get miss ($t)"
-	#out+=" \"< grep '$f' cucache.dat | perl -pe 's/^.*?-(\\\\d+)/\\\\1/' | grep Gets\" u 1:4 dt $dt lt 3 t '$tt' w linespoints,"
-	[ -n "$t" ] && tt="get ($t)"
-	out+=" \"< grep '$f' cucache.dat | perl -pe 's/^.*?-(\\\\d+)/\\\\1/' | grep Gets\" u 1:5 dt $dt lt 4 t '$tt' w linespoints,"
+	tt="get hit ($t)"
+	#out+=" \"< grep '$f' cucache.dat | perl -pe 's/^.*?-(\\\\d+)/\\\\1/' | grep Gets\" u 1:3 dt 1 pt 1 lt $lt t '$tt' w linespoints,"
+	tt="set ($t)"
+	out+=" \"< grep '$f' cucache.dat | perl -pe 's/^.*?-(\\\\d+)/\\\\1/' | grep Sets\" u 1:3 dt 2 pt 2 lt $lt t '$tt' w linespoints,"
+	tt="get miss ($t)"
+	#out+=" \"< grep '$f' cucache.dat | perl -pe 's/^.*?-(\\\\d+)/\\\\1/' | grep Gets\" u 1:4 dt 3 pt 3 lt $lt t '$tt' w linespoints,"
+	tt="get ($t)"
+	out+=" \"< grep '$f' cucache.dat | perl -pe 's/^.*?-(\\\\d+)/\\\\1/' | grep Gets\" u 1:5 dt 4 pt 4 lt $lt t '$tt' w linespoints,"
 
 	# Only "real" lines increment dash type
-	[ $dt -gt 1 ] && ((i=i*2))
+	[ $mc -eq 0 ] && ((i=i+1))
+	[ $mc -eq 1 ] && ((mi=mi-1))
 done
 
 # Strip trailing comma
